5242e2da03d9807d38093381a94b2ca370791948
Mariappan Ramasamy <2441529-Mariappan@users.noreply.gitlab.com>
2024-03-23 13:30:52 +0800

    Disable auto suspend in GDM

--- nixosModules/air/configuration.nix
+++ nixosModules/air/configuration.nix
@@ -31,6 +31,7 @@
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.autoSuspend = false;
   services.xserver.desktopManager.gnome.enable = true;
 
   # Enable XFCE4 Desktop Environment for RDP
