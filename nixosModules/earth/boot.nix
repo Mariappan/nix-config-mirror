3cf470a415fd0def4e18bdec6d95f75394e82b42
Mariappan Ramasamy <2441529-Mariappan@users.noreply.gitlab.com>
2024-04-17 18:42:02 +0800

    Add earth config

---
+++ nixosModules/earth/boot.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  # Bootloader.
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
+
+  boot.kernelModules = ["kvm-intel"];
+  boot.extraModulePackages = [];
+
+  boot.kernelParams = ["ip=dhcp"];
+
+  boot.initrd = {
+    availableKernelModules = ["xhci_pci" "r8152" "thunderbolt" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc"];
+    kernelModules = [];
+    systemd = {
+      enable = true;
+      users.root.shell = lib.mkIf (config.boot.initrd.systemd.enable) "/bin/systemd-tty-ask-password-agent";
+    };
+    network = {
+      enable = true;
+      ssh = {
+        enable = true;
+        port = 2022;
+        authorizedKeys = config.users.users.root.openssh.authorizedKeys.keys;
+        hostKeys = ["/etc/secrets/initrd/ssh_host_ed25519_key"];
+        shell = lib.mkIf (!config.boot.initrd.systemd.enable) "/bin/cryptsetup-askpass";
+      };
+    };
+  };
+}
