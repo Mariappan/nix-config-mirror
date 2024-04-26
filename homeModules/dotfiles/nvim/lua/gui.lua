42159ed0a490064912d40b941c4b94aaadeebc37
Mariappan Ramasamy <2441529-Mariappan@users.noreply.gitlab.com>
2024-04-26 11:41:31 +0800

    Add neophyte plugin

--- homeModules/dotfiles/nvim/lua/gui.lua
+++ homeModules/dotfiles/nvim/lua/gui.lua
@@ -2,7 +2,7 @@ local Util = require("utils")
 
 -- Set Gui options
 
-vim.opt.guifont = { "Operator Mono Lig,MesloLGS NF", "h14" }
+-- vim.opt.guifont = { "Operator Mono Lig,MesloLGS NF", "h14" }
 
 -- Set colorscheme
 vim.api.nvim_cmd({ cmd = "colorscheme", args = { "darcula" } }, {})
