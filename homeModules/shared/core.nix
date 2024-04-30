ff10aeebc0bb18f865c03329d57daead5e316612
Mariappan Ramasamy <2441529-Mariappan@users.noreply.gitlab.com>
2024-04-30 11:49:38 +0800

    Add htop config

--- homeModules/shared/core.nix
+++ homeModules/shared/core.nix
@@ -59,6 +59,19 @@
   programs.htop.settings = {
     hide_kernel_threads = true;
     hide_userland_threads = true;
+    hide_running_in_container = false;
+    shadow_other_users = true;
+    show_thread_names = true;
+    show_program_path = true;
+    highlight_base_name = true;
+    highlight_deleted_exe = true;
+    highlight_threads = true;
+    find_comm_in_cmdline = true;
+    strip_exe_from_cmdline = true;
+    show_merged_command = true;
+    header_margin = true;
+    cpu_count_from_one = 1;
+    tree_view = true;
   };
 
   home.stateVersion = "23.11";
