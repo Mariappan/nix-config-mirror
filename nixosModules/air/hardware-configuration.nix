719e38ef88618149d7b8ead7b32418d93911ddf6
Mariappan Ramasamy <2441529-Mariappan@users.noreply.gitlab.com>
2024-06-10 22:59:21 +0800

    Enable IPU6 camera

--- nixosModules/air/hardware-configuration.nix
+++ nixosModules/air/hardware-configuration.nix
@@ -32,6 +32,11 @@
 
   swapDevices = [];
 
+  hardware.ipu6 = {
+    enable = true;
+    platform = "ipu6ep";
+  };
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
