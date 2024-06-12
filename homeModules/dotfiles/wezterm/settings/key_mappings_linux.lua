c555a59b6b14cf3bcf8bee1f284ee2299b4cd324
Mariappan Ramasamy <2441529-Mariappan@users.noreply.gitlab.com>
2024-06-12 16:08:40 +0800

    wezterm: update more keys

--- homeModules/dotfiles/wezterm/settings/key_mappings_linux.lua
+++ homeModules/dotfiles/wezterm/settings/key_mappings_linux.lua
@@ -17,6 +17,17 @@ function module.apply(config)
       domain = "local",
     },
 
+    { key = "1", mods = "ALT", action = act.ActivateTab(0) },
+    { key = "2", mods = "ALT", action = act.ActivateTab(1) },
+    { key = "3", mods = "ALT", action = act.ActivateTab(2) },
+    { key = "4", mods = "ALT", action = act.ActivateTab(3) },
+    { key = "5", mods = "ALT", action = act.ActivateTab(4) },
+    { key = "6", mods = "ALT", action = act.ActivateTab(5) },
+    { key = "7", mods = "ALT", action = act.ActivateTab(6) },
+    { key = "8", mods = "ALT", action = act.ActivateTab(7) },
+    { key = "9", mods = "ALT", action = act.ActivateTab(8) },
+    { key = "0", mods = "ALT", action = act.ActivateTab(9) },
+
     { key = "J", mods = "CTRL|SHIFT", action = act.ActivateTabRelative(1) },
     { key = "K", mods = "CTRL|SHIFT", action = act.ActivateTabRelative(-1) },
     { key = "Enter", mods = "CTRL|SHIFT", action = act.ToggleFullScreen },
