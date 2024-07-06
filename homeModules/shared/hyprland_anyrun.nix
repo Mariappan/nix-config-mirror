5fcc48aea37cf5dd4fd9b44ea61eadd8a85cbfd3
Mariappan Ramasamy <142216110+kp-mariappan-ramasamy@users.noreply.github.com>
2024-07-06 17:26:12 +0800

    Fix hyperland anyrun

--- homeModules/shared/hyprland_anyrun.nix
+++ homeModules/shared/hyprland_anyrun.nix
@@ -6,8 +6,6 @@
         # An array of all the plugins you want, which either can be paths to the .so files, or their packages
         inputs.anyrun.packages.${pkgs.system}.applications
         inputs.anyrun.packages.${pkgs.system}.rink
-        inputs.anyrun.packages.${pkgs.system}.dictionary
-        inputs.anyrun.packages.${pkgs.system}.randr
         inputs.anyrun.packages.${pkgs.system}.shell
         # "${inputs.anyrun.packages.${pkgs.system}.anyrun-with-all-plugins}/lib/kidex"
       ];
@@ -23,6 +21,74 @@
       maxEntries = null;
     };
     extraCss = ''
+* {
+  all: unset;
+  font-size: 1.3rem;
+}
+
+#window,
+#match,
+#entry,
+#plugin,
+#main {
+  background: transparent;
+}
+
+#match.activatable {
+  border-radius: 8px;
+  padding: 0.3rem 0.9rem;
+  margin-top: 0.01rem;
+}
+#match.activatable:first-child {
+  margin-top: 0.7rem;
+}
+#match.activatable:last-child {
+  margin-bottom: 0.6rem;
+}
+
+#plugin:hover #match.activatable {
+  border-radius: 6px;
+  padding: 0.3rem;
+  margin-top: 0.01rem;
+  margin-bottom: 0;
+}
+
+#match:selected,
+#match:hover,
+#plugin:hover {
+  background: rgba(255, 255, 255, 0.1);
+}
+
+#entry {
+  background: rgba(255, 255, 255, 0.05);
+  border: 1px solid rgba(255, 255, 255, 0.1);
+  border-radius: 8px;
+  margin: 0.5rem;
+  padding: 0.3rem 1rem;
+}
+
+list > #plugin {
+  border-radius: 8px;
+  margin: 0 0.3rem;
+}
+list > #plugin:first-child {
+  margin-top: 0.3rem;
+}
+list > #plugin:last-child {
+  margin-bottom: 0.3rem;
+}
+list > #plugin:hover {
+  padding: 0.6rem;
+}
+
+box#main {
+  background: rgba(0, 0, 0, 0.9);
+  box-shadow:
+    inset 0 0 0 1px rgba(255, 255, 255, 0.1),
+    0 0 0 1px rgba(0, 0, 0, 0.5);
+  border-radius: 16px;
+  padding: 0.3rem;
+}
     '';
 
     # extraConfigFiles."some-plugin.ron".text = ''
