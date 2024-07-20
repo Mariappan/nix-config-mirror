2fd7a519e2bfc7bef52b3d80f5c3a9dac443cf68
Mariappan Ramasamy <142216110+kp-mariappan-ramasamy@users.noreply.github.com>
2024-07-20 18:20:04 +0800

    add kanshi for monitor management

--- homeModules/shared/hyprland.nix
+++ homeModules/shared/hyprland.nix
@@ -79,6 +79,38 @@
     '';
   };
 
+  services.kanshi = {
+    enable = true;
+    systemdTarget = "hyprland-session.target";
+
+    profiles = {
+      undocked = {
+        outputs = [
+          {
+            criteria = "eDP-1";
+            scale = 1.0;
+            status = "enable";
+          }
+        ];
+      };
+
+      home_office = {
+        outputs = [
+          {
+            criteria = "Dell Inc. DELL U2724DE 1LRK7P3";
+            position = "0,0";
+            mode = "2560x1440@60Hz";
+          }
+          {
+            criteria = "eDP-1";
+            status = "disable";
+          }
+        ];
+      };
+    };
+  };
+
+
   home.packages = [
     pkgs.hyprpicker
     pkgs.ianny
