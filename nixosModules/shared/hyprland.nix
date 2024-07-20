5ec4496183d33ca9d6744e8ec7d681305dd3e9ac
Mariappan Ramasamy <142216110+kp-mariappan-ramasamy@users.noreply.github.com>
2024-07-20 17:43:23 +0800

    Nixify all hypr configs

--- nixosModules/shared/hyprland.nix
+++ nixosModules/shared/hyprland.nix
@@ -2,37 +2,11 @@
   imports = [./xserver.nix];
 
   programs.hyprland.enable = true;
-  programs.hyprlock.enable = true;
-  services.hypridle.enable = true;
   programs.wshowkeys.enable = true;
-
-  programs.waybar.enable = true;
 
   services.logind.powerKey = "ignore";
 
   services.logind.lidSwitch = "suspend";
   services.logind.lidSwitchExternalPower = "ignore";
   services.logind.lidSwitchDocked = "lock";
-
-  environment.systemPackages = [
-    pkgs.hyprpicker
-    pkgs.swaynotificationcenter
-    pkgs.kitty
-    pkgs.ianny
-    pkgs.swww
-    pkgs.wl-clipboard
-
-    # Screenshot utility
-    pkgs.slurp
-    pkgs.grim
-
-    # Screenshot utility
-    pkgs.wf-recorder
-
-    # Image viewer
-    pkgs.imv
-
-    pkgs.wtype
-    pkgs.wayprompt
-  ];
 }
