c7762049472c04f134649ea7e650d5671cbe770b
Mariappan Ramasamy <142216110+kp-mariappan-ramasamy@users.noreply.github.com>
2024-08-28 12:21:50 +0800

    wezterm: remove git overlay
    Recent flake update breaks wezterm like below:
    https://github.com/wez/wezterm/issues/5990#issuecomment-2303362199
    
    For now, revert overlay and use the one in unstable branch.

--- flake.nix
+++ flake.nix
@@ -44,7 +44,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-parts.url = "github:hercules-ci/flake-parts";
-    wezterm.url = "github:wez/wezterm/b8f94c474ce48ac195b51c1aeacf41ae049b774e?dir=nix";
     nix-alien.url = "github:thiagokokada/nix-alien";
   };
 
