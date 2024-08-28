8bbe6a02e500d3d800f8c21d40ad1ee1fc7c1c88
Mariappan Ramasamy <142216110+kp-mariappan-ramasamy@users.noreply.github.com>
2024-08-28 12:36:51 +0800

    kanshi: replace deprecated profiles to settings

--- homeModules/shared/hyprland/kanshi.nix
+++ homeModules/shared/hyprland/kanshi.nix
@@ -3,9 +3,10 @@
     enable = true;
     systemdTarget = "hyprland-session.target";
 
-    profiles = {
-      undocked = {
-        outputs = [
+    settings = [
+      {
+        profile.name = "undocked";
+        profile.outputs = [
           {
             criteria = "eDP-1";
             scale = 1.0;
@@ -14,35 +15,35 @@
             mode = "1920x1200@60Hz";
           }
         ];
-      };
-
-      home_office = {
-        outputs = [
+      }
+      {
+        profile.name = "home_office";
+        profile.outputs = [
+          {
+            criteria = "eDP-1";
+            status = "disable";
+          }
           {
             criteria = "Dell Inc. DELL U2724DE 1LRK7P3";
             position = "0,0";
             mode = "2560x1440@60Hz";
           }
+        ];
+      }
+      {
+        profile.name = "office";
+        profile.outputs = [
           {
             criteria = "eDP-1";
             status = "disable";
           }
-        ];
-      };
-
-      office = {
-        outputs = [
           {
             criteria = "Beihai Century Joint Innovation Technology Co.,Ltd X340 PRO 165 Unknown";
             position = "0,0";
             mode = "3440x1440@50Hz";
           }
-          {
-            criteria = "eDP-1";
-            status = "disable";
-          }
         ];
-      };
-    };
+      }
+    ];
   };
 }
