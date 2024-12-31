7c0ab039060cba7e3030b47e1202e947dcc9f398
Mariappan Ramasamy <2441529-Mariappan@users.noreply.gitlab.com>
2024-12-31 10:43:19 +0800

    Disable GPG since it is running in headless mode temp

--- modules/homemanager/features/gpgagent.nix
+++ modules/homemanager/features/gpgagent.nix
@@ -1,8 +1,8 @@
 {pkgs, ...}: {
   services.gpg-agent = {
-    enable = true;
-    enableScDaemon = true;
-    enableSshSupport = true;
+    enable = false;
+    enableScDaemon = false;
+    enableSshSupport = false;
     extraConfig = "pinentry-program ${pkgs.wayprompt}/bin/pinentry-wayprompt";
   };
 }
