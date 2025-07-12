571e2e09bb6776a9b1a135957162f14b268ec87b
Mariappan Ramasamy <2441529-Mariappan@users.noreply.gitlab.com>
2025-07-12 11:39:13 +0800

    Use GTK theme for regreet

--- hosts/nixos/shared/hyprland.nix
+++ hosts/nixos/shared/hyprland.nix
@@ -3,6 +3,15 @@
   imports = [ ./xserver.nix ];
 
   programs.regreet.enable = true;
+  programs.regreet = {
+    settings = {
+      gtk.application_prefer_dark_theme = true;
+    };
+    cursorTheme.name = "Adwaita";
+    cursorTheme.package = pkgs.adwaita-icon-theme;
+    theme.name = "adw-gtk3-dark";
+    theme.package = pkgs.adw-gtk3;
+  };
   programs.hyprland.enable = true;
   programs.hyprland.withUWSM  = true;
   # Enable it for debug package
