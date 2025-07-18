b5ec2ee490242f1a5dd95dd06c1cba9dd2670d73
Mariappan Ramasamy <2441529-Mariappan@users.noreply.gitlab.com>
2025-07-18 11:26:20 +0800

    sound: enable wireplumber and disable monitoring libcamera

--- hosts/nixos/shared/sound.nix
+++ hosts/nixos/shared/sound.nix
@@ -1,10 +1,20 @@
-{
+{ pkgs, ... } : {
   # Enable sound with pipewire.
   security.rtkit.enable = true;
+  services.pulseaudio.enable = false;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    wireplumber.enable = true;
+
+    wireplumber.extraConfig."99-ignore-camera" = {
+      "wireplumber.profiles" = {
+        main = {
+          "monitor.libcamera" = "disabled";
+        };
+      };
+    };
   };
 }
