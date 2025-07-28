01aacd964ce75e3b151b6f0241824163691af701
Mariappan Ramasamy <2441529-Mariappan@users.noreply.gitlab.com>
2025-07-28 17:49:08 +0800

    Revert "Enable TLP in laptop config"
    This reverts commit cff3aa096b98544a410e3957a6c5c7909ce72ff8.

--- hosts/nixos/shared/laptop.nix
+++ hosts/nixos/shared/laptop.nix
@@ -4,45 +4,22 @@
   services.logind.lidSwitchDocked = "ignore";
 
   services.upower.enable = true;
-  services.thermald.enable = true;
+  # services.power-profiles-daemon.enable = true;
 
-  services.tlp = {
-    enable = true;
-    settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-
-      CPU_MIN_PERF_ON_AC = 0;
-      CPU_MAX_PERF_ON_AC = 100;
-      CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 20;
-
-      PLATFORM_PROFILE_ON_AC = "performance";
-      PLATFORM_PROFILE_ON_BAT = "low-power";
-
-      CPU_BOOST_ON_AC = 1;
-      CPU_BOOST_ON_BAT = 0;
-
-      CPU_HWP_DYN_BOOST_ON_AC = 1;
-      CPU_HWP_DYN_BOOST_ON_BAT = 0;
-
-      RUNTIME_PM_ON_AC = "on";
-      RUNTIME_PM_ON_BAT = "auto";
-
-      DEVICES_TO_DISABLE_ON_SHUTDOWN = "wwan";
-      DEVICES_TO_DISABLE_ON_LAN_CONNECT = "wifi";
-      DEVICES_TO_ENABLE_ON_LAN_DISCONNECT="wifi";
-
-      RUNTIME_PM_DRIVER_DENYLIST = "mei_me xhci_hcd";
-      SOUND_POWER_SAVE_ON_BAT = 10;
-      USB_AUTOSUSPEND = 1;
+  services.auto-cpufreq.enable = true;
+  services.auto-cpufreq.settings = {
+    battery = {
+       governor = "powersave";
+       turbo = "always";
+       # default performance balance_performance balance_power power
+       energy_performance_preference = "balance_power";
+    };
+    charger = {
+       governor = "performance";
+       turbo = "auto";
     };
   };
 
-  # services.power-profiles-daemon.enable = true;
   # powerManagement.enable = true;
   # powerManagement.powertop.enable = true;
 }
