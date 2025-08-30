e5f5703e5aad478d391f3e5523b21f4836b78be6
Mariappan Ramasamy <142216110+kp-mariappan-ramasamy@users.noreply.github.com>
2025-08-31 00:43:08 +0800

    ghostty config

--- modules/homemanager/shared/features/core.nix
+++ modules/homemanager/shared/features/core.nix
@@ -86,6 +86,44 @@
   };
 
   programs.ghostty.enable = true;
+  programs.ghostty.enableFishIntegration = true;
+  programs.ghostty.settings = {
+    theme = "GruvboxDark";
+
+    font-family = [
+      "Maple Mono"
+      "MesloLGS NF"
+    ];
+    font-family-bold = [
+      "Maple Mono Bold"
+      "MesloLGS NF"
+    ];
+    font-family-italic = [
+      "Script12 BT"
+      "MesloLGS NF"
+    ];
+    font-family-bold-italic = [
+      "Script12 BT"
+      "MesloLGS NF"
+    ];
+
+    font-size = 10.5;
+    adjust-underline-position = 4;
+    clipboard-read = "allow";
+    clipboard-write = "allow";
+    clipboard-trim-trailing-spaces = true;
+
+    background-blur = true;
+    background-opacity = 0.9;
+
+    app-notifications = "clipboard-copy";
+    window-padding-x = "5,5";
+
+    keybind = [
+      # Claude shift enter to enter
+      "shift+enter=text:\x1b\r"
+    ];
+  };
 
   programs.htop.enable = true;
   programs.htop.settings = {
