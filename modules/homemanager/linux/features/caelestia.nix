df7e41e5e9758406f411894c6f11f834ca65162e
Mariappan Ramasamy <142216110+kp-mariappan-ramasamy@users.noreply.github.com>
2025-09-04 09:45:37 +0800

    caelestia: add session gif

--- modules/homemanager/linux/features/caelestia.nix
+++ modules/homemanager/linux/features/caelestia.nix
@@ -1,11 +1,23 @@
-{ pkgs, ... }: {
-
+{ pkgs, lib, ... }:
+{
   programs.caelestia = {
     enable = true;
+    systemd = {
+      enable = true;
+      # default - graphical-session.target
+      target = "graphical-session.target";
+      environment = [ ];
+    };
     extraConfig = builtins.readFile ../../../../dotfiles/caelestia/shell.json;
     cli = {
       enable = true;
       extraConfig = builtins.readFile ../../../../dotfiles/caelestia/cli.json;
     };
+  };
+
+  home.activation = {
+    caelestiaAction = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      run cp ../../../../dotfiles/caelestia/goku.gif ~/.face.gif
+    '';
   };
 }
