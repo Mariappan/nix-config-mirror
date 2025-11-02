3c9d00ec3ce953cc7693b3c4b9d2e885e5573af0
Mariappan Ramasamy <2441529-Mariappan@users.noreply.gitlab.com>
2025-11-02 13:18:57 +0800

    air: change tmpfs size to 8G

--- hosts/nixos/air/boot.nix
+++ hosts/nixos/air/boot.nix
@@ -18,7 +18,7 @@
   boot.blacklistedKernelModules = [ "kvm-intel" ];
 
   boot.tmp.useTmpfs = true;
-  boot.tmp.tmpfsSize = "4G";
+  boot.tmp.tmpfsSize = "8G";
 
   boot.kernelParams = [
     "ip=dhcp"
