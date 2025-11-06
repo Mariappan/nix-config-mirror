edf5224378e15b8867bffd4a0f0fe78fbc86c3bf
Mariappan Ramasamy <2441529-Mariappan@users.noreply.gitlab.com>
2025-11-06 17:03:28 +0800

    gpg: make pinentry based on platform

--- modules/homemanager/features/gpgagent.nix
+++ modules/homemanager/features/gpgagent.nix
@@ -8,7 +8,13 @@
     enable = true;
     enableScDaemon = true;
     enableSshSupport = true;
-    extraConfig = "pinentry-program ${pkgs.wayprompt}/bin/pinentry-wayprompt";
+    extraConfig =
+      let
+        pinentryProgram = if pkgs.stdenv.isDarwin
+          then "${pkgs.pinentry_mac}/bin/pinentry-mac"
+          else "${pkgs.wayprompt}/bin/pinentry-wayprompt";
+      in
+        "pinentry-program ${pinentryProgram}";
     defaultCacheTtl = 3600;
     defaultCacheTtlSsh = 3600;
     maxCacheTtl = 14400;
