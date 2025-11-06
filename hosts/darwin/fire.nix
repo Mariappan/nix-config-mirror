53864862da760e4722b1832ff618da8b68f95519
Mariappan Ramasamy <2441529-Mariappan@users.noreply.gitlab.com>
2025-11-06 16:48:01 +0800

    Add moderntools to darwin:fire

--- hosts/darwin/fire.nix
+++ hosts/darwin/fire.nix
@@ -28,6 +28,7 @@ in
         nixma.dev.enable = true;
         nixma.git.enable = true;
         nixma.jujutsu.enable = true;
+        nixma.moderntools.enable = true;
 
         programs.git = {
           userName = "${userName}";
--- modules/homemanager/shared/features/gpgagent.nix
+++ modules/homemanager/shared/features/gpgagent.nix
@@ -9,5 +9,9 @@
     enableScDaemon = true;
     enableSshSupport = true;
     extraConfig = "pinentry-program ${pkgs.wayprompt}/bin/pinentry-wayprompt";
+    defaultCacheTtl = 3600;
+    defaultCacheTtlSsh = 3600;
+    maxCacheTtl = 14400;
+    maxCacheTtlSsh = 14400;
   };
 }
