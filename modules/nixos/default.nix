5796e6c3c297af3b1d7922cc19d817f4bffdea81
Mariappan Ramasamy <2441529-Mariappan@users.noreply.gitlab.com>
2025-11-19 22:36:30 +0800

    nixos module split into auto(features) and manual

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -10,28 +10,12 @@
 let
   cfg = config.nixma.nixos;
 
-  # Load all .nix files in this directory as features
-  # except for default.nix, common.nix
-  featuresDir = ./.;
-  allEntries = builtins.readDir featuresDir;
-
-  # Filter to get only .nix files, excluding special files
-  nixFiles = lib.filterAttrs (
-    name: type:
-      type == "regular"
-      && lib.hasSuffix ".nix" name
-      && name != "default.nix"       # default.nix is the current file, ignore it
-      && name != "common.nix"        # common.nix is included unconditionally
-      && !lib.hasPrefix "_" name
-  ) allEntries;
-
-  # Create features with nixma.nixos.{name}.enable pattern
-  features = libx.extendModules (name: {
-    extraOptions = lib.setAttrByPath
-      (lib.splitString "." "nixma.nixos.${name}.enable")
-      (lib.mkEnableOption "enable ${name} configuration");
-    configExtension = config: (lib.mkIf cfg.${name}.enable config);
-  }) (map (name: featuresDir + "/${name}") (builtins.attrNames nixFiles));
+  # Load features using the reusable function
+  features = libx.mkFeatures {
+    featuresDir = ./features;
+    inherit config lib;
+    optionPrefix = "nixma.nixos";
+  };
 in
 {
   # Always import common.nix (contains base config and options)
