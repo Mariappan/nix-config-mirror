ae115efeaf89a356a4e943a916558d9945342b39
Mariappan Ramasamy <2441529-Mariappan@users.noreply.gitlab.com>
2025-11-20 20:26:32 +0800

    Make water similar to fire

--- hosts/darwin/water.nix
+++ hosts/darwin/water.nix
@@ -1,55 +1,25 @@
 {
   lib,
-  pkgs,
   home-manager,
   outputs,
+  pkgs,
   ...
 }:
 {
-  imports = [ ];
+  # Set the primary user for this system
+  nixma.darwin.params.primaryUser = "maari";
 
-  users.users.maari = {
-    name = "maari";
-    home = "/Users/maari";
-    shell = "${pkgs.fish}/bin/fish";
+  # Configure users
+  nixma.darwin.users.maari = {
+    enable = true;
+    bundle = "water";
+    name = "Mariappan Ramasamy";
+    email = "maari@qq.com";
   };
-
-  home-manager.users.maari = {
-    imports = [
-      outputs.homeManagerModules.default
-      {
-        nixma.hm.core.enable = true;
-        nixma.hm.dev.enable = true;
-        nixma.hm.git.enable = true;
-        nixma.hm.jujutsu.enable = true;
-        nixma.hm.wezterm.enable = true;
-
-        programs.git = {
-          enable = true;
-          settings.user = {
-            name = "Mariappan Ramasamy";
-            email = "maari@qq.com";
-          };
-        };
-        programs.jujutsu.settings = {
-          user = {
-            email = "Mariappan Ramasamy";
-            name = "maari@qq.com";
-          };
-        };
-      }
-    ];
-  };
-
-  nix.settings.trusted-users = [ "maari" ];
-
-  # Needed for nix darwin temp. Will be replaced soon
-  system.primaryUser = "maari";
 
   # The platform the configuration will be used on.
   nixpkgs.hostPlatform = "aarch64-darwin";
 
-  nix.enable = false;
   # Enable x64 using rosetta
   nix.extraOptions = ''
     extra-platforms = x86_64-darwin aarch64-darwin
---
+++ modules/homemanager/bundle/water.nix
@@ -0,0 +1,12 @@
+{
+  pkgs,
+  lib,
+  ...
+}:
+{
+  nixma.hm.core.enable = true;
+  nixma.hm.dev.enable = true;
+  nixma.hm.git.enable = true;
+  nixma.hm.jujutsu.enable = true;
+  nixma.hm.wezterm.enable = true;
+}
