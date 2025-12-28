6f80bab9b421e8921a20109186d5a745d8df727a
Mariappan Ramasamy <2441529-Mariappan@users.noreply.gitlab.com>
2025-12-28 23:03:00 +0800

    Add new tetra nixos host config

---
+++ hosts/nixos/tetra/default.nix
@@ -0,0 +1,60 @@
+{ ... }:
+{
+  # Set the primary user for this system
+  nixma.nixos.params.primaryUser = "maari";
+
+  # Configure users
+  nixma.nixos.users.root.enable = true;
+  nixma.nixos.users.maari = {
+    enable = true;
+    email = "1221719+nappairam@users.noreply.github.com";
+    sshKeys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH3bwlIYLqj7YgfDNhFoAWgP5hg9+TOXmhnRZM9R8Bfi"
+    ];
+    bundle = "tetra";
+    gitSignByDefault = false;
+  };
+
+  # Hardware configuration
+  nixma.nixos.hardware = {
+    work.enable = false;
+    swap.enable = true;
+    cpu.vendor = "intel";
+  };
+
+  # Boot configuration
+  nixma.nixos.boot = {
+    kernelModules = [ "kvm-intel" ];
+    kernelPackage = "latest";
+    initrd.availableKernelModules = [
+      "nvme"
+      "xhci_pci"
+      "thunderbolt"
+      "ahci"
+      "usbhid"
+      "usb_storage"
+      "sd_mod"
+    ];
+  };
+
+  # Enable nixos features
+  nixma.nixos = {
+    "1password".enable = true;
+    docker.enable = true;
+    headless.enable = true;
+    # lanzaboote.enable = true;
+    manpages.enable = true;
+    niri.enable = true;
+    screenrecorder.enable = true;
+    sound.enable = true;
+    virtualbox.enable = true;
+  };
+
+  # System configs
+  networking.hostName = "tetra";
+
+  programs.mosh.enable = true;
+
+  # Host-specific networking configuration
+  time.timeZone = "Asia/Singapore";
+}
---
+++ modules/homemanager/bundle/tetra.nix
@@ -0,0 +1,35 @@
+{
+  inputs,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  nixma.hm.desktop.enable = true;
+  nixma.hm.moderntools.enable = true;
+  nixma.hm.git.enable = true;
+  nixma.hm.jujutsu.enable = true;
+  nixma.hm.rust.enable = true;
+  nixma.hm.dev.enable = true;
+  nixma.hm.debug.enable = true;
+  nixma.hm.gpgagent.enable = true;
+  nixma.hm.earthly.enable = true;
+  nixma.hm.zen-browser.enable = true;
+  nixma.hm.vivaldi.enable = true;
+
+  # Linux-specific configuration
+  nixma.hm.linux.niri.enable = lib.mkIf pkgs.stdenv.isLinux true;
+
+  home.packages = lib.mkIf pkgs.stdenv.isLinux [
+    pkgs.google-chrome
+    pkgs.spotify
+    pkgs.spotify-player
+    pkgs.obsidian
+    pkgs.nushell
+    pkgs.claude-code
+    pkgs.gpclient
+    pkgs.pavucontrol
+    pkgs.nixma.treewalker
+    pkgs.ookla-speedtest
+  ];
+}
