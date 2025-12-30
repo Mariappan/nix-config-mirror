47961976cfa165b6606bfea8e7762cb48356f7e1
Mariappan Ramasamy <2441529-Mariappan@users.noreply.gitlab.com>
2025-12-30 09:29:55 +0800

    Add zstd, mosh, tio to darwin#water

--- modules/homemanager/bundle/water.nix
+++ modules/homemanager/bundle/water.nix
@@ -1,8 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
 {
   nixma.hm.desktop.enable = true;
   nixma.hm.dev.enable = true;
   nixma.hm.git.enable = true;
   nixma.hm.jujutsu.enable = true;
   nixma.hm.wezterm.enable = true;
+  nixma.hm.moderntools.enable = true;
+
+  home.packages = [
+    pkgs.zstd
+    pkgs.mosh
+    pkgs.tio
+  ];
 }
