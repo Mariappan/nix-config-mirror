d7cec3a5837f77f0ddc8cac39a96ceec209527a9
Mariappan Ramasamy <2441529-Mariappan@users.noreply.gitlab.com>
2025-12-31 01:00:07 +0800

    rock480: add rock480 host

--- hosts/nixos/rock480/default.nix
+++ hosts/nixos/rock480/default.nix
@@ -1,5 +1,13 @@
-{ ... }:
 {
+  pkgs,
+  inputs,
+  ...
+}:
+{
+  imports = [
+    inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t480
+  ];
+
   # Set the primary user for this system
   nixma.nixos.params.primaryUser = "maari";
 
@@ -7,33 +15,28 @@
   nixma.nixos.users.root.enable = true;
   nixma.nixos.users.maari = {
     enable = true;
-    email = "1221719+nappairam@users.noreply.github.com";
+    email = "2441529-Mariappan@users.noreply.gitlab.com";
     sshKeys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH3bwlIYLqj7YgfDNhFoAWgP5hg9+TOXmhnRZM9R8Bfi"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ/gQaDONy7ryuW8R7tsnUxxpEoqQ1erZuM4KOb3VLAc maari@tetra"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINB3JnJ0u7pwetXhzAmskHUmxfQjcCtoyModO+IRKL89 homelab@1password"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFHYrhaeqkEaPmFxqfm8U26nBYU81cqPDTfd2PX96m0P xv@1password"
     ];
-    bundle = "tetra";
+    bundle = "rock480";
     gitSignByDefault = false;
   };
 
   # Hardware configuration
   nixma.nixos.hardware = {
-    work.enable = false;
-    swap.enable = true;
     cpu.vendor = "intel";
   };
 
   # Boot configuration
   nixma.nixos.boot = {
-    kernelModules = [ "kvm-intel" ];
     kernelPackage = "latest";
     initrd.availableKernelModules = [
-      "nvme"
       "xhci_pci"
-      "thunderbolt"
-      "ahci"
+      "nvme"
       "usbhid"
       "usb_storage"
       "sd_mod"
@@ -43,22 +46,29 @@
   # Enable nixos features
   nixma.nixos = {
     "1password".enable = true;
+    bluetooth.enable = true;
     docker.enable = true;
-    headless.enable = true;
-    # lanzaboote.enable = true;
+    fprint.enable = true;
+    laptop.enable = true;
     manpages.enable = true;
     niri.enable = true;
     nvidia.enable = true;
+    plymouth.enable = true;
     screenrecorder.enable = true;
     sound.enable = true;
-    virtualbox.enable = true;
+    zen-browser.enable = true;
   };
 
   # System configs
-  networking.hostName = "tetra";
+  networking.hostName = "rock480";
+
+  # Timezone
+  time.timeZone = "Asia/Singapore";
 
   programs.mosh.enable = true;
 
-  # Host-specific networking configuration
-  time.timeZone = "Asia/Singapore";
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  services.fwupd.enable = true;
 }
