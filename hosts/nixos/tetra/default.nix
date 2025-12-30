27d45e1030d7313b6d03a6fd4790e43a87d3a160
Mariappan Ramasamy <2441529-Mariappan@users.noreply.gitlab.com>
2025-12-31 01:17:17 +0800

    tetra: add ssh keys for maari

--- hosts/nixos/tetra/default.nix
+++ hosts/nixos/tetra/default.nix
@@ -10,6 +10,9 @@
     email = "1221719+nappairam@users.noreply.github.com";
     sshKeys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH3bwlIYLqj7YgfDNhFoAWgP5hg9+TOXmhnRZM9R8Bfi"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ/gQaDONy7ryuW8R7tsnUxxpEoqQ1erZuM4KOb3VLAc maari@tetra"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINB3JnJ0u7pwetXhzAmskHUmxfQjcCtoyModO+IRKL89 homelab@1password"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFHYrhaeqkEaPmFxqfm8U26nBYU81cqPDTfd2PX96m0P xv@1password"
     ];
     bundle = "tetra";
     gitSignByDefault = false;
